cda084a8920e32b7db056d50695640aebda1ff90
mrelectronst <fhkaraman@outlook.com>
2022-04-13 11:48:48 +0300

    Add project files.

---
+++ ConsoleAppRabbitMQ/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri(""); //write AMQP URL
+
+string messageRead;
+
+do
+{
+    Console.Write("Please write your message: ");
+    messageRead = Console.ReadLine();
+    if (messageRead != String.Empty && messageRead != null)
+    {
+        PublishMessage(messageRead);
+    }
+    else
+    {
+        Environment.Exit(0);
+    }
+
+} while (messageRead != null);
+
+void PublishMessage(string message)
+{
+    try
+    {
+        using (var connection = factory.CreateConnection())
+        {
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("queue-one", true, false, false);
+
+            var messageBody = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(string.Empty, "queue-one", null, messageBody);
+
+            Console.WriteLine($"'{message}' is sended");
+
+        }
+    }
+    catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+
+
+}
+
+
---
+++ SubscriberRabbitMQ/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri(""); //write AMQP URL
+
+using (var connection = factory.CreateConnection())
+{
+    var channel = connection.CreateModel();
+
+    channel.QueueDeclare("queue-one", true, false, false);
+
+    var subscriber = new EventingBasicConsumer(channel);
+
+    channel.BasicConsume("queue-one", true, subscriber);
+
+    subscriber.Received += (object? sender, BasicDeliverEventArgs e) =>
+    {
+        var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+        Console.WriteLine($"Received Message : {message}");
+    };
+}
